e0ac82a7a31501984f5157fa70a0b3a2c599d339
Rick Bennett <Rbennett1106@Gmail.com>
2021-08-22 16:30:45 -0400

    feat: add resources outline to status endpoint (API-68) (#11)
    Refactors the status endpoint response to include the resources that are currently live and available for consumption.

--- tests/Feature/Endpoints/V0/HealthCheckEndpointTest.php
+++ tests/Feature/Endpoints/V0/HealthCheckEndpointTest.php
@@ -9,14 +9,22 @@ class HealthCheckEndpointTest extends TestCase
     /** @test */
     public function it_can_check_the_server_status()
     {
+        $baseUrl = config('app.url');
+        $currentApiVersion = config('app.current_api_version');
+
         $response = $this->get('/v0');
 
         $response->assertStatus(200);
         $response->assertJson([
             'success' => true,
-            'message' => 'OK',
+            'message' => 'VIIIDB API is running.',
             'status_code' => 200,
-            'data' => null,
+            'data' => [
+                'message' => 'VIIIDB API is currently under construction and is subject to frequent major changes. The following resources are currently available for consumption.',
+                'resources' => [
+                    'seed_ranks' => "{$baseUrl}/v{$currentApiVersion}/seed-ranks",
+                ],
+            ],
         ]);
     }
 }
--- tests/Unit/Controllers/V0/HealthCheckControllerTest.php
+++ tests/Unit/Controllers/V0/HealthCheckControllerTest.php
@@ -10,15 +10,23 @@ class HealthCheckControllerTest extends TestCase
     /** @test */
     public function it_can_check_the_server_status()
     {
+        $baseUrl = config('app.url');
+        $currentApiVersion = config('app.current_api_version');
+
         $controller = new HealthCheckController();
         $response = $controller->status();
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertArraySubset([
             'success' => true,
-            'message' => 'OK',
+            'message' => 'VIIIDB API is running.',
             'status_code' => 200,
-            'data' => null,
+            'data' => [
+                'message' => 'VIIIDB API is currently under construction and is subject to frequent major changes. The following resources are currently available for consumption.',
+                'resources' => [
+                    'seed_ranks' => "{$baseUrl}/v{$currentApiVersion}/seed-ranks",
+                ],
+            ],
         ], $response->getData(true));
     }
 }
