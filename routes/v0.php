adc577fb55383ff120161b3c9a002ad9f66643ac
Rick Bennett <Rbennett1106@Gmail.com>
2021-08-22 15:55:28 -0400

    refactor: update route parameter name for seed ranks (API-70) (#10)
    This simply updates the route parameter name for the `/seed-ranks` endpoints from `seed_rank` to `rank`.

--- routes/v0.php
+++ routes/v0.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\V0\SeedRankController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/')->uses([HealthCheckController::class, 'status']);
-Route::resource('seed-ranks', SeedRankController::class)->only(['index', 'show']);
+Route::resource('seed-ranks', SeedRankController::class)->only(['index', 'show'])->parameters(['seed-ranks' => 'rank']);
