22d2f7b894ef81f05d1234e0ecfb6b1ddbb92424
Rick Bennett <Rbennett1106@Gmail.com>
2021-09-22 23:01:33 -0400

    refactor: extract base class for resource controllers (API-84) (#37)
    Resource controllers are beginning to duplicate the same logic across classes. This logic is largely the same across each class, albeit with slightly different variable names for clarity and slightly different resources being injected. Instead of duplicating this logic across all classes, a base resource controller has been created which allows for customization via extension for each specific resource.

---
+++ app/Http/Controllers/ResourceController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contracts\Services\ModelService;
+use App\Contracts\Transformers\RecordTransformer;
+use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ResourceController extends Controller
+{
+    use ApiResponse;
+
+    /**
+     * Instance of the ModelService.
+     *
+     * @var ModelService
+     */
+    protected $service;
+
+    /**
+     * Instance of the RecordTransformer.
+     *
+     * @var RecordTransformer
+     */
+    protected $transformer;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request The HTTP request from the client
+     *
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        $records = $this->service->all($request);
+
+        return $this->respondWithSuccess(
+            'Successfully retrieved data.',
+            $this->transformer->transformCollection($records)
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Request $request The HTTP request from the client
+     * @param string  $id      The ID of the requested record
+     *
+     * @return JsonResponse
+     */
+    public function show(Request $request, string $id): JsonResponse
+    {
+        return $this->respondWithSuccess(
+            'Successfully retrieved data.',
+            $this->transformer->transformRecord(
+                $this->service->findOrFail($id, $request)
+            )
+        );
+    }
+}
--- app/Http/Controllers/V0/StatusEffectController.php
+++ app/Http/Controllers/V0/StatusEffectController.php
@@ -3,30 +3,12 @@
 namespace App\Http\Controllers\V0;
 
 use App\Contracts\Services\StatusEffectService;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ResourceController;
 use App\Http\Transformers\V0\StatusEffectTransformer;
-use App\Traits\ApiResponse;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class StatusEffectController extends Controller
+class StatusEffectController extends ResourceController
 {
-    use ApiResponse;
-
-    /**
-     * Instance of the StatusEffectService.
-     *
-     * @var StatusEffectService
-     */
-    protected $statusEffectService;
-
-    /**
-     * Instance of the StatusEffectTransformer.
-     *
-     * @var StatusEffectTransformer
-     */
-    protected $statusEffectTransformer;
-
     /**
      * Create a new StatusEffectController instance.
      *
@@ -35,42 +17,7 @@ class StatusEffectController extends Controller
      */
     public function __construct(StatusEffectService $statusEffectService, StatusEffectTransformer $statusEffectTransformer)
     {
-        $this->statusEffectService = $statusEffectService;
-        $this->statusEffectTransformer = $statusEffectTransformer;
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @param Request $request The HTTP request from the client
-     *
-     * @return JsonResponse
-     */
-    public function index(Request $request): JsonResponse
-    {
-        $records = $this->statusEffectService->all($request);
-
-        return $this->respondWithSuccess(
-            'Successfully retrieved data.',
-            $this->statusEffectTransformer->transformCollection($records)
-        );
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param Request $request        The HTTP request from the client
-     * @param string  $statusEffectId The ID of the requested StatusEffect
-     *
-     * @return JsonResponse
-     */
-    public function show(Request $request, string $statusEffectId): JsonResponse
-    {
-        return $this->respondWithSuccess(
-            'Successfully retrieved data.',
-            $this->statusEffectTransformer->transformRecord(
-                $this->statusEffectService->findOrFail($statusEffectId, $request)
-            )
-        );
+        $this->service = $statusEffectService;
+        $this->transformer = $statusEffectTransformer;
     }
 }
